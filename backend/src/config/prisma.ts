b5a9a12e23f5939a8b4551b1cc6c625d21d3c8ad
bishalghale98 <bishalghle@gmail.com>
2026-03-03 23:43:41 +0545

    feat: Implement initial backend service with Prisma schema, database connection, and externalized environment variables.

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from './env';
 
@@ -6,3 +6,17 @@ const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
+
+
+
+export const dbConnect = async () => {
+    try {
+        console.log('🔄 Connecting to database...');
+        await prisma.$connect();
+        console.log('✅ Database connected successfully');
+    } catch (error) {
+        console.error('❌ Database connection failed');
+        console.error('📛 Error:', error);
+        process.exit(1);
+    }
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,15 @@ import http from 'http';
 import app from './app';
 import { env } from './config/env';
 import { initSocketIO } from './socket/socket';
+import { dbConnect } from './config/prisma';
 
 const server = http.createServer(app);
 
 // Initialize Socket.IO
 initSocketIO(server);
 
-server.listen(env.PORT, () => {
+server.listen(env.PORT, async () => {
+    await dbConnect();
     console.log(`🚀 Server running on port ${env.PORT}`);
     console.log(`📡 Environment: ${env.NODE_ENV}`);
     console.log(`🔗 API: http://localhost:${env.PORT}/api/v1`);
